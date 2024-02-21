baba4db292eb9be83b82eef2cb53d8911c13e161
Jason Schulte <jasonschulte@gmail.com>
2024-02-21 11:53:43 -0600

    Update AH Bot parameters

--- data/sql/db-world/base/auctionhouse_bot.sql
+++ data/sql/db-world/base/auctionhouse_bot.sql
@@ -1,3 +1,7 @@
-UPDATE `mod_auctionhousebot` SET `minitems` = '40000', `maxitems` = '50000', `percentwhitetradegoods` = '40', `percentgreentradegoods` = '20', `percentwhiteitems` = '7', `percentgreenitems` = '12' WHERE `auctionhouse` = '2';
-UPDATE `mod_auctionhousebot` SET `minitems` = '40000', `maxitems` = '50000', `percentwhitetradegoods` = '40', `percentgreentradegoods` = '20', `percentwhiteitems` = '7', `percentgreenitems` = '12' WHERE `auctionhouse` = '6';
-UPDATE `mod_auctionhousebot` SET `minitems` = '40000', `maxitems` = '50000', `percentwhitetradegoods` = '40', `percentgreentradegoods` = '20', `percentwhiteitems` = '7', `percentgreenitems` = '12' WHERE `auctionhouse` = '7';
+-- Set basic AH Bot params
+UPDATE `mod_auctionhousebot` SET `minitems` = '50000', `maxitems` = '60000', `percentwhitetradegoods` = '40', `percentgreentradegoods` = '20', `percentwhiteitems` = '7', `percentgreenitems` = '12' WHERE `auctionhouse` = '2';
+UPDATE `mod_auctionhousebot` SET `minitems` = '50000', `maxitems` = '60000', `percentwhitetradegoods` = '40', `percentgreentradegoods` = '20', `percentwhiteitems` = '7', `percentgreenitems` = '12' WHERE `auctionhouse` = '6';
+UPDATE `mod_auctionhousebot` SET `minitems` = '50000', `maxitems` = '60000', `percentwhitetradegoods` = '40', `percentgreentradegoods` = '20', `percentwhiteitems` = '7', `percentgreenitems` = '12' WHERE `auctionhouse` = '7';
+
+-- Disable items from AH Bot
+REPLACE INTO `mod_auctionhousebot_disabled_items` (`item`) VALUES ('11099');  -- Dark Iron Ore (OLD)
